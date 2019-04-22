2dc08dd28f56dcdaa90ba8be2f20910526727dfe
Kayla <6lackmacaroon@gmail.com>
2019-04-22 15:48:06 -0600

    complete footer icons stretch

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import HeaderContainer from './components/HeaderComponents/HeaderContainer';
 import CardContainer from './components/CardComponents/CardContainer';
-
+import Foot from './components/FooterComponents/Footer';
 import './App.css';
 
 const App = () => {
@@ -9,13 +9,8 @@ const App = () => {
     <div>
       <HeaderContainer />
       <CardContainer />
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
-    </div>
+      <Foot />
+      </div>
   );
 };
 
---
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.css';
+
+const Foot = () => {
+      return <div class='container foots'>
+                  <i class="far fa-comment fa-2x"></i>
+                  <i class="fas fa-retweet fa-2x"></i>
+                  <i class="far fa-heart fa-2x"></i>
+                  <i class="far fa-envelope fa-2x"></i>
+            </div>
+}
+
+export default Foot;
