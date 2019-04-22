0da99e2fee6d880a218019e985f82c569cf37728
Kayla <6lackmacaroon@gmail.com>
2019-04-22 14:06:12 -0600

    finish header components

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,9 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () =>{
+      return <h3>Lambda School <span>@LambdaSchool howDoITimestamp</span></h3>
+      
+}
+
+export default HeaderTitle;
