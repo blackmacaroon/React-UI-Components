a5b7cb2a59bf4f3417b1e8004b1cd58083e8bf41
Kayla <6lackmacaroon@gmail.com>
2019-04-23 17:18:40 -0600

    mvp

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,53 @@
+
 import React from 'react';
 import './Button.css';
+
+
+
+
+const DivideButton = {
+    buttonStyle: 'actionButton',
+    text: '/'
+};
+
+const TimesButton = {
+    buttonStyle: 'actionButton',
+    text: 'x'
+};
+
+const AddButton = {
+      buttonStyle: 'actionButton',
+      text: '+'
+  };
+
+const MinusButton = {
+  buttonStyle: 'actionButton',
+  text: '-'
+};
+
+const EqualsButton = {
+    buttonStyle: 'actionButton',
+    text: '='
+  };
+
+const FunctionButton = (props) => {
+    return (
+        <div className="functionalButtons">
+            <button className={props.button.buttonStyle}>{props.button.text}</button>
+        </div>
+    )
+}
+
+const FunctionButtons = () => {
+    return (
+        <div className="functions">
+            <FunctionButton button ={DivideButton}/>
+            <FunctionButton button ={TimesButton}/>
+            <FunctionButton button ={AddButton}/>
+            <FunctionButton button ={MinusButton}/>
+            <FunctionButton button ={EqualsButton}/>
+        </div>
+    );
+}
+
+export default FunctionButtons;
--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,86 @@
 import React from 'react';
 import './Button.css';
+
+const ClearButton = {
+      buttonStyle: 'numberButton',
+      text: 'clear'
+};
+const SevenButton = {
+    buttonStyle: 'numberButton',
+    text: '7'
+};
+const EightButton = {
+    buttonStyle: 'numberButton',
+    text: '8'
+};
+const NineButton = {
+    buttonStyle: 'numberButton',
+    text: '9'
+};
+const FourButton = {
+    buttonStyle: 'numberButton',
+    text: '4'
+};
+const FiveButton = {
+    buttonStyle: 'numberButton',
+    text: '5'
+};
+const SixButton = {
+    buttonStyle: 'numberButton',
+    text: '6'
+};
+const OneButton = {
+    buttonStyle: 'numberButton',
+    text: '1'
+};
+const TwoButton = {
+    buttonStyle: 'numberButton',
+    text: '2'
+};
+const ThreeButton = {
+    buttonStyle: 'numberButton',
+    text: '3'
+};
+const ZeroButton = {
+    buttonStyle: 'numberButton',
+    text: '0'
+};
+
+
+const NumberButton = (props) => {
+    return (
+        <div className="numericButtons">
+            <button className={props.button.buttonStyle}>{props.button.text}</button>
+        </div>
+    )
+}
+
+const NumberButtons = () => {
+    return (
+        <div className="numbers">
+            <div className='longButton'>
+                  <NumberButton button ={ClearButton}/>
+            </div>
+            <div className='toprow'>
+                  <NumberButton button ={SevenButton}/>
+                  <NumberButton button ={EightButton}/>
+                  <NumberButton button ={NineButton}/>
+            </div>
+            <div className='midrow'>
+                  <NumberButton button ={FourButton}/>
+                  <NumberButton button ={FiveButton}/>
+                  <NumberButton button ={SixButton}/>
+            </div>
+            <div className='bottomrow'>
+                  <NumberButton button ={OneButton}/>
+                  <NumberButton button ={TwoButton}/>
+                  <NumberButton button ={ThreeButton}/>
+            </div>
+            <div className='longButton'>
+                  <NumberButton button ={ZeroButton}/>
+            </div>
+        </div>
+    );
+}
+
+export default NumberButtons;
